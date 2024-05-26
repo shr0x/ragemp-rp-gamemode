24a5b1e0acccbbe9d09211249fb5237184fb942d
shr0x <44136562+shr0x@users.noreply.github.com>
2024-05-26 12:55:20 +0200

    tweak updates on use item

--- source/server/modules/inventory/UseItem.module.ts
+++ source/server/modules/inventory/UseItem.module.ts
@@ -6,9 +6,8 @@
  * @returns void
  */
 export const useInventoryItem = async (player: PlayerMp, data: string): Promise<void> => {
-    if (!player.character) return;
     try {
-        if (!mp.players.exists(player) || !player.character.inventory) return;
+        if (!mp.players.exists(player) || !player.character || !player.character.inventory) return;
         const { item, source }: { item: RageShared.Interfaces.Inventory.IInventoryItem; source: { component: string; slot: number } } = JSON.parse(data);
 
         switch (item.type) {
