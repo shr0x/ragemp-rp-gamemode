35013508a8b7a3748b4e6411840f20af7d37b95f
shr0x <44136562+shr0x@users.noreply.github.com>
2024-06-28 20:12:55 +0200

    eslint config updates

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,12 +1,26 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tseslint from "@typescript-eslint/eslint-plugin";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
-
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      globals: globals.browser,
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  tseslint.configs.recommended,
   pluginReactConfig,
 ];
