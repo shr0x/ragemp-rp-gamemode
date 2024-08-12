916d21952a740b158116f2900bfba418550b0629
shr0x <44136562+shr0x@users.noreply.github.com>
2024-08-12 22:07:33 +0200

    Reset page context

--- frontend/src/PageContext.tsx
+++ frontend/src/PageContext.tsx
@@ -6,7 +6,7 @@ const PageContext = createContext({
 });
 
 export const PageProvider: FC<{ children: ReactNode }> = ({ children }) => {
-    const [page, setPage] = useState<string | null>("settings");
+    const [page, setPage] = useState<string | null>(null);
 
     const handleSetPage = useCallback((newPage: string | null) => setPage(newPage), []);
 
